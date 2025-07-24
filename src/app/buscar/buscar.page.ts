7318752810a8ddadcd0a5754cf15ce35ce9b2c37
Benji-1009 <112718865+Benji-1009@users.noreply.github.com>
2025-07-23 21:29:01 -0600

    se añaden archivos que se modificaron para el front

--- src/app/buscar/buscar.page.ts
+++ src/app/buscar/buscar.page.ts
@@ -1,7 +1,9 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
-import { IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar, IonAvatar } from '@ionic/angular/standalone';
+import { IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar, IonAvatar, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { IonicModule } from '@ionic/angular'
+import { IonicModule } from '@ionic/angular';
+import { triangle, ellipse, square, homeOutline, homeSharp, star, personSharp, personCircleOutline, informationSharp, searchSharp } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
@@ -12,13 +14,14 @@ NgModule({
   templateUrl: './buscar.page.html',
   styleUrls: ['./buscar.page.scss'],
   standalone: true,
-    imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCardContent, IonCard, IonInput, IonItem, IonList, IonFooter, IonTabs, IonTabBar,IonTabButton, IonIcon, IonLabel, IonAvatar]
+    imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCardContent, IonCard, IonInput, IonItem, IonList, IonFooter, IonTabs, IonTabBar,IonTabButton, IonIcon, IonLabel, IonAvatar, IonButton]
 
 })
 export class BuscarPage {
 
 
   constructor() { 
+        addIcons({ triangle, ellipse, square, homeSharp, homeOutline, star, personCircleOutline, personSharp, informationSharp, searchSharp });
     
   }
 

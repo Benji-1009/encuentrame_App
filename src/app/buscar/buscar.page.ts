d853a2a6a3098184c82586678dd65caae65fa0d4
Benji-1009 <112718865+Benji-1009@users.noreply.github.com>
2025-07-23 20:16:48 -0600

    modified:   src/app/buscar/buscar.page.html 	modified:   src/app/buscar/buscar.page.ts 	modified:   src/app/login/tab1.page.html

--- src/app/buscar/buscar.page.ts
+++ src/app/buscar/buscar.page.ts
@@ -1,19 +1,26 @@
-import { Component} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import { IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar, IonAvatar } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonicModule } from '@ionic/angular'
+
+NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
 
 @Component({
   selector: 'app-buscar',
   templateUrl: './buscar.page.html',
   styleUrls: ['./buscar.page.scss'],
   standalone: true,
-    imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCardContent, IonCard, IonInput, IonItem, IonList, IonFooter, IonTabs, IonTabBar,IonTabButton, IonIcon, IonLabel],
+    imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCardContent, IonCard, IonInput, IonItem, IonList, IonFooter, IonTabs, IonTabBar,IonTabButton, IonIcon, IonLabel, IonAvatar]
+
 })
 export class BuscarPage {
 
-  constructor() { }
+
+  constructor() { 
+    
+  }
 
 
 }
